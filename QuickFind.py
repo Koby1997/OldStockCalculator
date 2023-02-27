3d3f8984b1305e652e9a8deb9c0af4cbbab97497
Koby Miller <KobyLMiller@outlook.com>
2023-02-26 18:47:17 -0700

    Create QuickFind.py file to find what stocks have available dates to use

--- QuickFind.py
+++ QuickFind.py
@@ -4,7 +4,7 @@ import datetime
 
 
 # List of tickers to make
-all_tickers = ["aapl","aapl", "ge", "aapl", "amzn"]#,"GOOGL","MSFT","JPM","JNJ","V","BABA","WMT","PG","NFLX","DIS"]
+all_tickers = ["GE", "AAPL", "AMZN","GOOGL","MSFT","JPM","JNJ","V","BABA","WMT","PG","NFLX","DIS"]
 ticker_success_count = 0
 
 #loop through all stocks listed above
@@ -17,34 +17,29 @@ for symbol in all_tickers:
         continue
 
 
-    # Print the earnings call dates
-    earnings_call_dates_and_data = stock.get_earnings_dates(limit=150)
-    # print(earnings_call_dates_and_data)
+    # earnings_call_dates_and_data = stock.get_earnings_dates(limit=150)
+    # # print(earnings_call_dates_and_data)
 
-    # gives a DatetimeIndex which cannot be used with .timedelta
-    earnings_call_dates = earnings_call_dates_and_data.index
+    # # gives a DatetimeIndex which cannot be used with .timedelta
+    # earnings_call_dates = earnings_call_dates_and_data.index
 
-    # change format to datetime to allow the used of .timedelta
-    dates = earnings_call_dates.to_pydatetime()
-    #There are multiple future dates added. So we want to get rid of those
-    dates = dates[10:]
-    print(dates)
+    # # change format to datetime to allow the used of .timedelta
+    # dates = earnings_call_dates.to_pydatetime()
 
-    print("ight here we go    ", symbol)
+
 
     dividends = stock.dividends
 
     # Get all of the ex-dividend dates
     ex_dividend_dates = dividends.index.to_pydatetime()
 
-    # Print the ex-dividend dates
-    print(f"The ex-dividend dates for {symbol} are:")
-    for date in ex_dividend_dates:
-        print(date)
+    # # Print the ex-dividend dates
+    # print(f"The ex-dividend dates for {symbol} are:")
+    # for date in ex_dividend_dates:
+    #     print(date)
 
+    if len(ex_dividend_dates) > 0:
+        print(symbol, "        good")
+    else:
+        print(symbol, " BAD")
 
-
-    # dividend_calendar = stock.calendar
-    # ex_d_d = dividend_calendar.iloc[0]["ExDividendDate"]
-    # print("!!!!!!!!!!!!!!!!!!!!")
-    # print(ex_d_d)
